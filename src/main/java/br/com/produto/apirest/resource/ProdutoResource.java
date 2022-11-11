c9f86abc51519f9cd1b0806ef427861da8a91298
rafaelcn1 <rafaelcn1@hotmail.com>
2022-11-11 18:02:39 -0300

    Erro caused by: java.lang.nullpointerexception: null at springfox.documentation.spring.web.webmvcpatternsrequestconditionwrapper.getpatterns

--- src/main/java/br/com/produto/apirest/resource/ProdutoResource.java
+++ src/main/java/br/com/produto/apirest/resource/ProdutoResource.java
@@ -3,6 +3,7 @@ package br.com.produto.apirest.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.produto.apirest.models.Produto;
 import br.com.produto.apirest.reposiotory.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /*
  * Essa classe é que vai receber as requisições http
@@ -22,6 +25,8 @@ import br.com.produto.apirest.reposiotory.ProdutoRepository;
 
 @RestController // Anotação para aplicação REST
 @RequestMapping(value = "/api") // Criação da uri padrão, ex: /api/produto
+@Api(value = "Api Rest Produtos") // Nome definido da Api
+@CrossOrigin(origins = "*") // Para liberar quem usarar essa API, o * faz que todo mundo possa acessar, caso queira restrigir bastar colocar o dominio, exemplo: http://clientedaapi.com.br
 public class ProdutoResource {
 
 	@Autowired // Se conectar ao banco de dados
@@ -30,6 +35,7 @@ public class ProdutoResource {
 	// Metodo (via GET) para listar todos os produtos do banco de dados, teste pelo
 	// postman
 	@GetMapping("/produtos")
+	@ApiOperation(value = "Lista todos os produtos cadastrados")
 	public List<Produto> listaProdutos() {
 		return produtoRepository.findAll();
 	}
@@ -37,24 +43,28 @@ public class ProdutoResource {
 	// Metodo (via GET) para listar apenas 1 produto no banco de dados, teste pelo
 	// postman
 	@GetMapping("/produto/{id}")
+	@ApiOperation(value = "Lista um produto unico")
 	public Produto findById(@PathVariable(value = "id") Long id) { //@PathVariable vai ser para passarmos o valor do id na url
 		return produtoRepository.findById(id).orElse(null);
 	}
 
 	// Metodo post para salvar um produto novo
 	@PostMapping("/produto")
+	@ApiOperation(value = "Salva um produto")
 	public Produto salvarProduto(@RequestBody Produto produto) { // @RequestBody, a requisição vai pelo corpo da requisição, então o metodo vai receber o produto
 		return produtoRepository.save(produto);
 	}
 	
 	// Metodo para deletar um produto
 	@DeleteMapping("/produto") // Metodo para deletar uma linha na tabela produto
+	@ApiOperation(value = "Deleta um produto")
 	public void deletarProduto(@RequestBody Produto produto) { // @RequestBody, a requisição vai pelo corpo da requisição igual ao salvar, então o metodo vai receber o produto para poder deletar
 		produtoRepository.delete(produto);
 	}
 	
 	// Metodo post para salvar um produto novo
 	@PutMapping("/produto")
+	@ApiOperation(value = "Atualiza um produto")
 	public Produto atualizarProduto(@RequestBody Produto produto) { // @RequestBody, a requisição vai pelo corpo da requisição, então o metodo vai receber o produto
 		return produtoRepository.save(produto); // porque também é save? é porque como ele vai recer o produto completo pelo corpo da requisição @RequestBody, vai também o ID, então vai ser feito um "marge" 
 	}
