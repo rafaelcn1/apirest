0b5595f8d2bd325f6e21fe7528b632691a9fdcc9
rafaelcn1 <rafaelcn1@hotmail.com>
2022-11-11 11:30:41 -0300

    Implementando os métodos DELETE e PUT

--- src/main/java/br/com/produto/apirest/resource/ProdutoResource.java
+++ src/main/java/br/com/produto/apirest/resource/ProdutoResource.java
@@ -3,9 +3,11 @@ package br.com.produto.apirest.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +45,18 @@ public class ProdutoResource {
 	@PostMapping("/produto")
 	public Produto salvarProduto(@RequestBody Produto produto) { // @RequestBody, a requisição vai pelo corpo da requisição, então o metodo vai receber o produto
 		return produtoRepository.save(produto);
+	}
+	
+	// Metodo para deletar um produto
+	@DeleteMapping("/produto") // Metodo para deletar uma linha na tabela produto
+	public void deletarProduto(@RequestBody Produto produto) { // @RequestBody, a requisição vai pelo corpo da requisição igual ao salvar, então o metodo vai receber o produto para poder deletar
+		produtoRepository.delete(produto);
+	}
+	
+	// Metodo post para salvar um produto novo
+	@PutMapping("/produto")
+	public Produto atualizarProduto(@RequestBody Produto produto) { // @RequestBody, a requisição vai pelo corpo da requisição, então o metodo vai receber o produto
+		return produtoRepository.save(produto); // porque também é save? é porque como ele vai recer o produto completo pelo corpo da requisição @RequestBody, vai também o ID, então vai ser feito um "marge" 
 	}
 	
 }
